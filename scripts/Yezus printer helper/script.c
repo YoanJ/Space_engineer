1cba99b8179a6abd41108b1532f5d4f8e817f91d
YoanJ <yoan.janvier@gmail.com>
2025-10-12 23:54:36 +0200

    Add initial README and script files for Yezus printer helper

---
+++ scripts/Yezus printer helper/script.c
@@ -0,0 +1,4 @@
+// The goal of this script is to help Yezus printer to setup the printer
+// It will identify printer components based on their postion and orientation relative to the rotor, tagged with [YPH]
+// The printer is made of a main rotor, hosting a subgrid with projector and a component.
+// At the bottom of the rotor, two groups of pistons pouching outward of the rotor 
